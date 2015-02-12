dc9448d62c27320b7dcc4424c8d235319a5e4d8f
Eduardo Quagliato <eduardo@quagliato.me>
2015-02-12 15:56:00 -0200

    Edital - subscription confirmation e-mail

--- custom/view/edital_confirmacao.php
+++ custom/view/edital_confirmacao.php
@@ -18,7 +18,7 @@
     $subject = DEFAULT_EMAIL_SUBJECT." / ".$edital->get("nome")." - Confirmação de inscrição";
     $mailHTML = "";
     $mailHTML .= "<br /><br /><h2>Parabéns! Sua submissão ao edital de {$edital->get('nome')} foi recebida.</h2>
-        <p>Caso tenha dúvidas, entre em contato através de <a href=\"mailto:editais@nsp2015.com.br\" alt=\"E-mail para editais@nsp2015.com.br\" title=\"E-mail para editais@nsp2015.com.br\" href=\"mailto:editais@nsp2015.com.br\">editais@nsp2015.com.br</a>.</p>";
+        <p>Caso tenha dúvidas, entre em contato através de <a href=\"mailto:".DEFAULT_EMAIL_FROM_EDITAIS."\" alt=\"E-mail para ".DEFAULT_EMAIL_FROM_EDITAIS."\" title=\"E-mail para ".DEFAULT_EMAIL_FROM_EDITAIS."\" href=\"mailto:".DEFAULT_EMAIL_FROM_EDITAIS."\">".DEFAULT_EMAIL_FROM_EDITAIS."</a>.</p>";
 
     $mailResult = $notification->sendEmail($usuario->get('email'), $subject, $mailHTML, DEFAULT_EMAIL_FROM_EDITAIS);
 
@@ -30,7 +30,7 @@
             <section class="wrapper center">
                 <p><i class="fa fa-check-circle-o" style="color:#1DA075; font-size:200px; text-align:center;"></i>
                 <h2>Parabéns! Sua submissão ao edital de <?=$edital->get('nome')?> foi recebida.</h2>
-                <p class="mt20">Caso tenha dúvidas, entre em contato através de <a href="mailto:editais@nsp2015.com.br" alt="E-mail para editais@nsp2015.com.br" title="E-mail para editais@nsp2015.com.br" href="mailto:editais@nsp2015.com.br">editais@nsp2015.com.br</a>.</p>
+                <p class="mt20">Caso tenha dúvidas, entre em contato através de <a href="mailto:<?=DEFAULT_EMAIL_FROM_EDITAIS?>" alt="E-mail para <?=DEFAULT_EMAIL_FROM_EDITAIS?>" title="E-mail para <?=DEFAULT_EMAIL_FROM_EDITAIS?>" href="mailto:<?=DEFAULT_EMAIL_FROM_EDITAIS?>"><?=DEFAULT_EMAIL_FROM_EDITAIS?></a>.</p>
                 <p class="mt20 center"><a href="<?=APP_URL?>/dashboard" alt="Voltar para o painel" title="Voltar para o painel" class="submit positive">Voltar para o painel</a></p>
             </section>
         </main>
