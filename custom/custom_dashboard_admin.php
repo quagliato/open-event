991da29d4c84abc2ac7ae4f7a2723c83cdead3fe
Eduardo Quagliato <eduardo@quagliato.me>
2015-02-13 03:32:36 -0200

    Implementing sections on admin dashboard.

--- custom/custom_dashboard_admin.php
+++ custom/custom_dashboard_admin.php
@@ -1,28 +1,46 @@
 <h1 class="center">Admin</h1>
+<section id="cadastros">
+    <header>
+        <h2 class="clickable" data-target="#cadastros .content">Cadastros</h2>
+    </header>
+    <div class="content">
+        <div class="menu_block third fleft">
+            <h3>Editais</h3>
+            <ul>
+                <li><a href="<?=APP_URL?>/admin/edital">Cadastrar</a></li>
+                <li><a href="<?=APP_URL?>/admin/edital/action/list">Listar</a></li>
+                <li><a href="<?=APP_URL?>/admin/edital/buscar">Buscar</a></li>
+            </ul>
+        </div>
 
-<div class="menu_block third fleft">
-    <h2>Editais</h2>
-    <ul>
-        <li><a href="<?=APP_URL?>/admin/edital">Cadastrar</a></li>
-        <li><a href="<?=APP_URL?>/admin/edital/action/list">Listar</a></li>
-        <li><a href="<?=APP_URL?>/admin/edital/buscar">Buscar</a></li>
-    </ul>
-</div>
+        <div class="menu_block third fleft">
+            <h3><!--i class="fa fa-question"></i--> Perguntas</h3>
+            <ul>
+                <li><a href="<?=APP_URL?>/admin/pergunta">Cadastrar</a></li>
+                <li><a href="<?=APP_URL?>/admin/pergunta/action/list">Listar</a></li>
+                <li><a href="<?=APP_URL?>/admin/pergunta/buscar">Buscar</a></li>
+            </ul>
+        </div>
 
-<div class="menu_block third fleft">
-    <h2><!--i class="fa fa-question"></i--> Perguntas</h2>
-    <ul>
-        <li><a href="<?=APP_URL?>/admin/pergunta">Cadastrar</a></li>
-        <li><a href="<?=APP_URL?>/admin/pergunta/action/list">Listar</a></li>
-        <li><a href="<?=APP_URL?>/admin/pergunta/buscar">Buscar</a></li>
-    </ul>
-</div>
+        <div class="menu_block third fleft">
+            <h3><!--i class="fa fa-list-ol"></i-->Valores Possíveis</h3>
+            <ul>
+                <li><a href="<?=APP_URL?>/admin/valor-possivel">Cadastrar</a></li>
+                <li><a href="<?=APP_URL?>/admin/valor-possivel/action/list">Listar</a></li>
+                <li><a href="<?=APP_URL?>/admin/valor-possivel/buscar">Buscar</a></li>
+            </ul>
+        </div>
+    </div>
+</section>
 
-<div class="menu_block third fleft">
-    <h2><!--i class="fa fa-list-ol"></i-->Valores Possíveis</h2>
-    <ul>
-        <li><a href="<?=APP_URL?>/admin/valor-possivel">Cadastrar</a></li>
-        <li><a href="<?=APP_URL?>/admin/valor-possivel/action/list">Listar</a></li>
-        <li><a href="<?=APP_URL?>/admin/valor-possivel/buscar">Buscar</a></li>
-    </ul>
-</div>
+<script>
+$('.clickable').bind('click', function(event){
+    event.preventDefault();
+    var target = $(this).attr("data-target");
+    if (target.hasClass("hidden")) {
+        target.removeClass("hidden");
+    } else {
+        target.addClass("hidden");
+    }
+});
+</script>
