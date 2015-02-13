bf6b22acf6a0e8630b8711c778751db6632b78ec
Eduardo Quagliato <eduardo@quagliato.me>
2015-02-13 03:20:53 -0200

    Implementing real average time on Edital answering page.

--- custom/dao/RespostaEditalDAO.php
+++ custom/dao/RespostaEditalDAO.php
@@ -18,5 +18,31 @@ class RespostaEditalDAO extends GenericDAO {
 
         return false;
     }
+
+    public function getAverageTime($idEdital) {
+        if (!isset($idEdital) || !$idEdital) {
+            return false;
+        }
+
+        $answeringCount = 1;
+        $answeringTime = 0;
+        $respostasEdital = $this->selectAll("RespostaEdital", "id_edital = $idEdital");
+        if ($respostasEdital) {
+            if (!is_array($respostasEdital)) $respostasEdital = array($respostasEdital);
+
+            foreach ($respostasEdital as $respostaEdital) {
+                $begin = new DateTime($respostaEdital->get('dt_inicio_resposta'));
+                $end = new DateTime($respostaEdital->get('dt_fim_resposta'));
+
+                $diff = $end->diff($begin);
+                if (intval($diff->format("%h")) > 1) $answeringTime += intval($diff->format("%h") * 60;
+                $answeringTime += intval($diff->format("%i"));
+
+                $answeringCount++;
+            }
+        }
+
+        return intval($answeringTime / $answeringCount);
+    }
 }
 ?>
--- custom/view/edital.php
+++ custom/view/edital.php
@@ -4,6 +4,7 @@
 
     $genericDAO = new GenericDAO;
     $editalDAO = new EditalDAO;
+    $respostaEditalDAO = new respostaEditalDAO;
 
     $idEdital = $_GET['id'];
     $edital = $genericDAO->selectAll("Edital", "id = $idEdital");
@@ -29,7 +30,7 @@
                             <!-- <i class="fa fa-times f-negative"></i>&nbsp;<?=Utils::sqlTimestamp2BrFormat($edital->get('dt_fechamento'))?> -->
                             <strong>Fechamento deste edital:</strong> <?=Utils::sqlTimestamp2BrFormat($edital->get('dt_fechamento'))?>
                         </span>
-                        <span class="fright right"><i class="fa fa-clock-o"></i> <?=$edital->get('tempo_preenchimento')?> minuto(s)</span>
+                        <span class="fright right"><i class="fa fa-clock-o"></i> <?=$respostaEditalDAO->getAverageTime($edital->get('id')?> minuto(s)</span>
                     </div>
                 </div>
                 
