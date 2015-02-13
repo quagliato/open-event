6da463b792197720cb1460785beafd92dfb45e44
Eduardo Quagliato <eduardo@quagliato.me>
2015-02-13 02:28:10 -0200

    CSS updates on Edital dashboard time relative note.

--- custom/custom_dashboard.php
+++ custom/custom_dashboard.php
@@ -14,7 +14,18 @@
         foreach ($editais as $edital) :
             $hasAnsweredEdital = $respostaEditalDAO->hasAnsweredEdital($usuario->get('id'), $edital->get('id'));
 ?>
-        <li><span class="fleft left upper"><?=$edital->get('nome')?><span class="no-transform note"><?=$hasAnsweredEdital ? "Você respondeu esse edital há ".Utils::getRelativeDate(new DateTime($hasAnsweredEdital->get('dt_fim_resposta'))) : ''?></span></span> <span class="fright right"><a href="<?=APP_URL?>/edital?id=<?=$edital->get('id')?>">Inscrição</a></span></li>
+        <li>
+            <span class="fleft left upper"><?=$edital->get('nome')?>
+                <?php if ($hasAnsweredEdital) : ?>
+                <span class="no-transform note">
+                    Você respondeu esse edital há <?=Utils::getRelativeDate(new DateTime($hasAnsweredEdital->get('dt_fim_resposta')))?>.
+                </span>
+                <?php endif; ?>
+            </span>
+            <span class="fright right">
+                <a href="<?=APP_URL?>/edital?id=<?=$edital->get('id')?>">Inscrição</a>
+            </span>
+        </li>
 <?php
         endforeach;
 ?>
