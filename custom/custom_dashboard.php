d6247f9c2eef8f11def325ce9643962f92ffea67
Eduardo Quagliato <eduardo@quagliato.me>
2015-05-07 18:49:34 -0300

    Dashboard.

--- custom/custom_dashboard.php
+++ custom/custom_dashboard.php
@@ -1,4 +1,22 @@
 <h2>Inscrições</h2>
+<style>
+table.noline { border: none; }
+table.noline tr { border: none; }
+table.noline tr td{ border: none; }
+table.noline thead { border: none; }
+table.noline thead td{ border: none; }
+.bb1s{ border-bottom: #666666 1px solid !important; }
+.bt1s{ border-top: #666666 1px solid !important; }
+</style>
+<table class="noline">
+    <thead>
+        <td class="center tenth">Código</td>
+        <td class="fourth">Descrição</td>
+        <td class="right tenth">Valor</td>
+        <td class="right tenth">Isenção</td>
+        <td class="third">Status</td>
+        <td class="tenth">Pagamento</td>
+    </thead>
 <?php
     $genericDAO = new GenericDAO;
     $user = $genericDAO->selectAll("Usuario", "id = ".$_SESSION['user_id']);
@@ -6,9 +24,33 @@
     if ($transactions) {
         if (!is_array($transactions)) $transactions = array($transactions);
         foreach ($transactions as $transaction) {
+            $payment  = false;
+            if ($transaction->get('id_last_payment') == '' || is_null($transaction->get('id_last_payment'))) {
+                $payments = $genericDAO->selectAll("TransactionPayment", "id_transaction = ".$transaction->get('id')." ORDER BY dt_payment DESC");
+                if ($payments) {
+                    if (!is_array($payments)) {
+                        $payment = $payments;
+                    } else {
+                        $payment = $payment[0];
+                    }
+                }
+            } else {
+                $payment = $genericDAO->selectAll("TransactionPayment", "id = ".$transaction->get('id_last_payment'));
+            }
+            $transactionStatus = "";
+            if ($transaction->get('status') == 0) $transactionStatus = "Aguardando Confirmação";
+            if ($transaction->get('status') == 1) $transactionStatus = "Inscrição Confirmada";
+            if ($transaction->get('status') == 2) $transactionStatus = "Credenciado";
+            if ($transaction->get('status') == 3) $transactionStatus = "Inscrição Cancelada";
 ?>
-    <hr>
-    <p>Inscrição <?=$transaction->get('id')?> / Valor Total: R$ <strong><?=$transaction->get('total_value')?></strong><?=$transaction->get('value_exemption') > 0 ? 'Isenção: R$ <strong>'.$transaction->get('value_exemption').'</strong>' : ''?></p>
+    <tr>
+        <td class="center"><?=$transaction->get('id')?></td>
+        <td></td>
+        <td class="right">R$ <?=$transaction->get('total_value')?></td>
+        <td class="right">R$ <?=$transaction->get('value_exemption')?></td>
+        <td><?=$transactionStatus?></td>
+        <td><?=$payment ? '<a href="'.$payment->get('info').'">Pagar</a>' : ''?></td>
+    </tr>
 <?php
             $transactionItems = $genericDAO->selectAll("TransactionItem", "id_transaction = ".$transaction->get('id'));
             if ($transactionItems) {
@@ -16,19 +58,28 @@
                 foreach ($transactionItems as $transactionItem) {
                     $product = $genericDAO->selectAll("Product", "id = ".$transactionItem->get('id_product'));
 ?>
-                <p>&nbsp;&nbsp;&nbsp;<?=$transactionItem->get('id')?> - <?=$product->get('description')?> / Valor Total: R$ <strong><?=$transactionItem->get('vl_item')?></strong><?=$transaction->get('value_exemption') > 0 ? 'Isenção: R$ <strong>'.$transactionItem->get('vl_exemption').'</strong>' : ''?></p>
+                <tr>
+                    <td></td>
+                    <td><?=$product->get('description')?></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
 <?php
                 }
             }
         }
     }
 ?>
-<hr>
+</table>
 <?php if (!isMaxReached()) : ?>
-    <p><a href="<?=APP_URL?>/pacotes">Faça sua inscrição ou compre pacotes adicionais.</a></p>
+    <p><h3><a href="<?=APP_URL?>/pacotes">Faça sua inscrição ou compre pacotes adicionais.</a></h3></p>
 <?php else: ?>
-    <p>Inscrições fechadas no momento.</p>
+    <p><h3>Inscrições fechadas no momento.</h3></p>
 <?php endif; ?>
+
+<hr>
 
 <?php
     $editalDAO = new editalDAO;
@@ -36,7 +87,7 @@
     $editais = $editalDAO->getOpenEdital();
     if ($editais) :
 ?>
-    <h2>Editais abertos</h2>
+    <h2>Editais</h2>
     <p class="light">Os editais são o melhor caminho para você participar ativamente na construção de um encontro único. Leia o manual com as instruções para cada edital e inscreva-se em quantos desejar. <strong>Estamos torcendo por você (:</strong></p>
     <ul id="edital_list" class="wrapper">
 <?php
