29c1b8534b3ad668bbd7f8515b13c7c4f46421a4
Eduardo Quagliato <eduardo@quagliato.me>
2015-09-09 17:33:41 -0300

    Custom job to confirm all exempted transaction

--- custom/custom_jobs.php
+++ custom/custom_jobs.php
@@ -29,4 +29,11 @@
 
   $result = $genericDAO->updateWithFields($transactionPayment, array('status'), "status = 0 AND (SELECT COUNT(t.status) FROM transaction t WHERE t.id = id_transaction AND (t.status = 0 OR t.status = 3)) > 0 AND dt_payment < '".$limit->format('Y-m-d H:i:s')."'");
 
+//******************************************************************************
+// Confirm all transactions approved on edital
+  $transaction = new Transaction();
+  $transaction->set('status', 1);
+
+  $result = $genericDAO->updateWithFields($transaction, array('status'), "total_value = 0 AND value_exemption > 0";
+
 ?>
